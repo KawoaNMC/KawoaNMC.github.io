02845b2545a1436e96945052d41d888277df7a9b
Kawoa NMC <manhcuong0978481376@gmail.com>
2021-01-10 10:30:11 +0700

    Sua menu cua khoi project

--- 1.js
+++ 1.js
@@ -9,12 +9,22 @@
  	  	$('body,html').animate({scrollTop:$('#khoisanpham').offset().top},700);
  	  	return false ; 
  	  
- 	 })
-	  $(".cm").click(function(){
+ 	})
+	$(".cm").click(function(){
 		$('body,html').animate({scrollTop:$('#comment').offset().top},700);
 		return false ; 
 	
    })
 
+   	// js cho menu khối project
+   	let link = $('.link');
+	for (let j = 0; j < link.length; j++) {
+   		link[j].addEventListener('click', function () {
+      		for (let i = 0; i < link.length; i++) {
+         		link[i].classList.remove('active');
+     		}
+      		this.classList.add('active');
+   		});
+	};
 })  
  

b87ce712b18f147cb4aaf602a0ad56bc746eff9a
Kawoa NMC <manhcuong0978481376@gmail.com>
2021-01-11 20:22:51 +0700

    Xong phan San Pham

--- 1.js
+++ 1.js
@@ -137,5 +137,184 @@
 			$('.sp6 .sub-title').animate({ opacity: 0, "margin-left": "-80px"}, 400)
 		}
 	)
+
+	// Game Rò mìn
+	$('.sp1 .imgProduct').click(
+		function() {
+			$('#thongTinGameRoMin').css({"z-index": "2000", opacity: 1})
+		},
+	)
+
+	$('#thongTinGameRoMin .thongTinSP .thongTinSPRight .cancleThongTinSP').click (
+		function() {
+			$('#thongTinGameRoMin').css({"z-index": "-1", opacity: 0})
+		}
+	)
+
+	$('.sp4 .imgProduct').click(
+		function() {
+			$('#thongTinGameRoMin').css({"z-index": "2000", opacity: 1})
+		},
+	)
+
+	$('#thongTinGameRoMin .thongTinSP .thongTinSPRight .cancleThongTinSP').click (
+		function() {
+			$('#thongTinGameRoMin').css({"z-index": "-1", opacity: 0})
+		}
+	)
+
+	// Game rắn Săn Mồi
+	$('.sp5 .imgProduct').click(
+		function() {
+			$('#thongTinGameRanSanMoi').css({"z-index": "2000", opacity: 1})
+		},
+	)
+
+	$('#thongTinGameRanSanMoi .thongTinSP .thongTinSPRight .cancleThongTinSP').click (
+		function() {
+			$('#thongTinGameRanSanMoi').css({"z-index": "-1", opacity: 0})
+		}
+	)
+
+   // Shop basketball
+	$('.sp3 .imgProduct').click(
+		function() {
+			$('#thongTinShopBasketball').css({"z-index": "2000", opacity: 1})
+		},
+	)
+
+	$('#thongTinShopBasketball .thongTinSP .thongTinSPRight .cancleThongTinSP').click (
+		function() {
+			$('#thongTinShopBasketball').css({"z-index": "-1", opacity: 0})
+		}
+	)
+
+	$('.sp6 .imgProduct').click(
+		function() {
+			$('#thongTinShopBasketball').css({"z-index": "2000", opacity: 1})
+		},
+	)
+
+	$('#thongTinShopBasketball .thongTinSP .thongTinSPRight .cancleThongTinSP').click (
+		function() {
+			$('#thongTinShopBasketball').css({"z-index": "-1", opacity: 0})
+		}
+	)
+
+	// Website
+	$('.sp2 .imgProduct').click(
+		function() {
+			$('#thongTinWebsite').css({"z-index": "2000", opacity: 1})
+		},
+	)
+
+	$('#thongTinWebsite .thongTinSP .thongTinSPRight .cancleThongTinSP').click (
+		function() {
+			$('#thongTinWebsite').css({"z-index": "-1", opacity: 0})
+		}
+	)
+
+	// chuyển ảnh sang trái sang phải cho project website
+	var listImgWebsite = [
+		"images/imgProject/Shop1.PNG ",
+		"images/imgProject/Shop2.PNG",
+		"images/imgProject/Shop3.PNG ",
+		"images/imgProject/Shop4.PNG",
+		"images/imgProject/Shop5.PNG ",
+		"images/imgProject/Shop6.PNG",
+		"images/imgProject/Shop7.PNG ",
+		"images/imgProject/Shop8.PNG",
+		"images/imgProject/Shop9.PNG ",
+		"images/imgProject/Shop10.PNG",
+		"images/imgProject/Shop11.PNG ",
+		"images/imgProject/Shop12.PNG",
+		"images/imgProject/Shop13.PNG ",
+		"images/imgProject/Shop14.PNG",
+		"images/imgProject/Shop15.PNG ",
+		"images/imgProject/Shop16.PNG"
+	]
+	var id = 0;
+	$('#thongTinShopBasketball .thongTinSP .thongTinSPRight .sangTrai').click(
+		function() {
+			var slider = document.getElementById("imgSPShopBasketball");
+			id--;
+			if(id < 0) {
+				id = listImgWebsite.length - 1;
+			}
+			slider.src = listImgWebsite[id];
+		}
+	)
+	$('#thongTinShopBasketball .thongTinSP .thongTinSPRight .sangPhai').click(
+		function() {
+			var slider = document.getElementById("imgSPShopBasketball");
+			id++;
+			if(id >= listImgWebsite.length) {
+				id = 0;
+			}
+			slider.src = listImgWebsite[id];
+		}
+	)
+
+	// chuyển ảnh sang trái sang phải cho project Rắn Săn Mồi
+	var listImgRanSanMoi = [
+		"images/imgProject/Snake1.png",
+		"images/imgProject/Snake2.png",
+		"images/imgProject/Snake3.png",
+		"images/imgProject/Snake4.png",
+		"images/imgProject/Snake5.png",
+	]
+	var id = 0;
+	$('#thongTinGameRanSanMoi .thongTinSP .thongTinSPRight .sangTrai').click(
+		function() {
+			var slider = document.getElementById("imgSPRanSanMoi");
+			id--;
+			if(id < 0) {
+				id = listImgRanSanMoi.length - 1;
+			}
+			slider.src = listImgRanSanMoi[id];
+		}
+	)
+	$('#thongTinGameRanSanMoi .thongTinSP .thongTinSPRight .sangPhai').click(
+		function() {
+			var slider = document.getElementById("imgSPRanSanMoi");
+			id++;
+			if(id >= listImgRanSanMoi.length) {
+				id = 0;
+			}
+			slider.src = listImgRanSanMoi[id];
+		}
+	)
+
+	
+	// chuyển ảnh sang trái sang phải cho project Rò Mìn
+	var listImgRoMin = [
+		"images/imgProject/RoMin1.png",
+		"images/imgProject/RoMin2.png",
+		"images/imgProject/RoMin3.png",
+		"images/imgProject/RoMin4.png",
+		"images/imgProject/RoMin5.png",
+		"images/imgProject/RoMin6.png",
+	]
+	var id = 0;
+	$('#thongTinGameRoMin .thongTinSP .thongTinSPRight .sangTrai').click(
+		function() {
+			var slider = document.getElementById("imgSPRoMin");
+			id--;
+			if(id < 0) {
+				id = listImgRoMin.length - 1;
+			}
+			slider.src = listImgRoMin[id];
+		}
+	)
+	$('#thongTinGameRoMin .thongTinSP .thongTinSPRight .sangPhai').click(
+		function() {
+			var slider = document.getElementById("imgSPRoMin");
+			id++;
+			if(id >= listImgRoMin.length) {
+				id = 0;
+			}
+			slider.src = listImgRoMin[id];
+		}
+	)
 })  
  
